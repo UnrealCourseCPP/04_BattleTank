855a2088c141009a070901e812735af0d274df83
Spiros Makris <makris.spiros@gmail.com>
2016-06-29 22:15:51 +0300

    BT_115: Using virtual and override

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,25 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Player Controller BeginPlay()"));
+
+	ATank* ControlledTank = GetControlledTank();
+
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Controlled Tank: %s"), *ControlledTank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("TankPC can't find possessed Tank!"));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
