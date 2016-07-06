60fe07c44ac7c81d9f4f9f674eeb2bbcdd3ccc94
Spiros Makris <makris.spiros@gmail.com>
2016-07-06 21:46:03 +0300

    BT_141: Projectile Movement Components

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -61,15 +61,15 @@ void ATank::SetTurretReference(UTankTurret* TurretToSet)
 
 void ATank::Fire()
 {
-	UE_LOG(LogTemp, Warning, TEXT("%f: Tank firing!"), GetWorld()->GetTimeSeconds());
+	
 
 	if (!Barrel) { return; }
 
 	// Spawn a projectile at the socket location
-	GetWorld()->SpawnActor<AProjectile>(
-		ProjectileBlueprint,
-		Barrel->GetSocketLocation(FName("Projectile")),
-		Barrel->GetSocketRotation(FName("Projectile"))
-		);
+	AProjectile* Projectile = GetWorld()->SpawnActor<AProjectile>(
+							ProjectileBlueprint,
+							Barrel->GetSocketLocation(FName("Projectile")),
+							Barrel->GetSocketRotation(FName("Projectile")));
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
 
