7cdf9952d65131308c96ca65e1b0c591e7a9c3f2
Spiros Makris <makris.spiros@gmail.com>
2016-07-13 23:09:58 +0300

    BT_145: Setup Tank Throttles

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	UE_LOG(LogTemp, Warning, TEXT("%f: %s Throttle: %f"), GetWorld()->GetTimeSeconds(), *GetName(), Throttle);
+
+	// TODO(spiros): Clamp actual throttle value so player can't overdrive
+}
+
+
