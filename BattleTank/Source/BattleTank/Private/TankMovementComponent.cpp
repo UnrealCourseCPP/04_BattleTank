172664093a7dda1e7d365a140280c143a9c08026
Spiros Makris <makris.spiros@gmail.com>
2016-07-22 01:02:56 +0300

    BT_155: CrossProduct() Vector Operator

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -23,7 +23,7 @@ void UTankMovementComponent::IntendMoveForward(float Throw)
 void UTankMovementComponent::IntendTurnRight(float Throw)
 {
 	if (!LeftTrack || !RightTrack) { return; }
-
+	UE_LOG(LogTemp, Warning, TEXT("Turning with throw = %f"), Throw);
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
 }
@@ -31,15 +31,23 @@ void UTankMovementComponent::IntendTurnRight(float Throw)
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
 	// No need to call Super() as we are replacing the functionality
-
-	// Get normalized direction we want to move to
-	FVector AIForwardIntention = MoveVelocity.GetSafeNormal();
+	
+	//UE_LOG(LogTemp, Warning, TEXT("In RequestDirectMove"));
 
 	// Get tank's forward vector (also normalised)
 	FVector TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	
+	// Get normalized direction we want to move to
+	FVector AIForwardIntention = MoveVelocity.GetSafeNormal();
+	//UE_LOG(LogTemp, Warning, TEXT("MoveVelocity = %s"), *AIForwardIntention.ToString() );
 
+	float ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
 	// Use result of dot product of AIForwardIntention and TankForward as Throw
-	IntendMoveForward(FVector::DotProduct(AIForwardIntention, TankForward));
+	IntendMoveForward(ForwardThrow);
+
+	float RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
+	UE_LOG(LogTemp, Warning, TEXT("Cross.Z = %f"), RightThrow);
+	IntendTurnRight(RightThrow);
 
 	//UE_LOG(LogTemp, Warning, TEXT("In UTankMovementComponent::RequestDirectMove(), from tank: %s , MoveVelocity: %s"), *GetOwner()->GetName(), *AIForwardIntention.ToString() );
 }
